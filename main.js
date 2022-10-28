82ce6b8989a78ba796078e58d2b33e699d687c68
TAFA Aurel <aurel.tafa@soprasteria.com>
2022-10-28 17:41:34 +0200

    add

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 
 const link = "http://localhost:8080/api/todo";
-const link2 = "http://localhost:8080/api/todo/delete"
+const link2 = "http://localhost:8080/api/todo/delete";
 let content = document.querySelector(".container");
 let lista = document.querySelector(".lista");
 chiamataTasks();
@@ -15,16 +15,14 @@ function chiamataTasks() {
         dataContent = response.data
         lista.innerHTML = ""
         for (let i = 0; i < dataContent.length; i++) {
-           lista.insertAdjacentHTML("afterbegin",`<li id="${dataContent[i].taskId}" class="lista-todo">${dataContent[i].taskName}<button type="button" class="btn btn-primary ps-4"> Cancella </button>  </li>`)
+           lista.insertAdjacentHTML("afterbegin",`<li class="lista-todo">${dataContent[i].taskName}<button id="${dataContent[i].taskId}" type="button" class="btn btn-primary ps-4"> Cancella </button>  </li>`)
     
         
         
         }
         let elementsArray = document.querySelectorAll(".lista-todo")
         elementsArray.forEach(function(elem) {
-            elem.addEventListener("input", function() {
-                rimuoviTask(event)
-            });
+            elem.addEventListener("click",  rimuoviTask);
         });
        
     
@@ -35,12 +33,20 @@ function chiamataTasks() {
 };
 
 function rimuoviTask(event) {
-
-    console.log("ciao");
-        event.preventDefault();
-       
-    
-    
+    event.preventDefault();
+    let taskId = event.target.id
+   console.log(event.target.id);  
+   console.log(link2+"/"+taskId);  
+    axios.delete(link2+"/"+taskId)
+   .then((response) => {
+       chiamataTasks();
+   }) 
+   .catch((error) => {
+    // codice da eseguire quando la richiesta non va a buon fine
+    // error conterrà i dettagli della risposta 
+    console.log(error);
+    divErr.innerHTML="Impossibile inserire il corso: "+error.message;
+});
 
     
 }
